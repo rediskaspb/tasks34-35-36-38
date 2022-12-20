4cd672776078135185bea957a4f4c01b73fa4cc5
Anastasiya <9399023@mail.ru>
2022-12-20 16:32:40 +0300

    add task 34-38

---
+++ t33/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+// Массивы к задачам 32 и 33 можно сделать рандомными. В задаче 33 на экран выводится ТОЛЬКО одно сообщение: либо "да", либо "нет".
+
+// void[] GetArray(int[] array);
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 101);
+    // Console.Write(array[i] + ", ");
+    // }
+// Console.WriteLine(String.Join(", ", array));
+// }
+
+void FindDigit(int [] array, int digit)
+{
+    bool find=false;
+    for(int i =0; i < array.Length; i++)
+    {
+        if(array[i]==digit)
+        {
+            find=true;
+        }
+    }
+}
+
+
